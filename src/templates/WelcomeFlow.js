25375e503cc1289c41f8c54beb2da69b86ec4bd9
arielAayala <ariel170802@gmail.com>
2024-12-07 21:00:59 -0300

    some change in the flows

--- src/templates/WelcomeFlow.js
+++ src/templates/WelcomeFlow.js
@@ -1,17 +1,40 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
 import { textQuery } from "../config/dialogFlow.js";
 
-const welcomeFlow = addKeyword(EVENTS.WELCOME).addAnswer(
-	[`🙌 Hola`, "Soy un asistente virtual😁", "¿En que puedo ayudarte?"],
-	{
+const welcomeFlow = addKeyword(EVENTS.WELCOME)
+	.addAnswer([`🙌 Hola`, "Soy un asistente virtual😁"])
+	.addAnswer(
+		[
+			"Te pediré algunos datos",
+			"Necesitaré que escribas solamente nombre completo para continuar",
+		],
+		{ capture: true },
+		async (ctx, { state, flowDynamic }) => {
+			const { body, from } = ctx;
+			await state.update({ nombreCompleto: body, numeroTelefono: from });
+			await flowDynamic("Gracias por darme tu nombre");
+		}
+	)
+	.addAnswer(
+		[
+			"Necesitaré que escribas solamente tu documento completo sin puntos para continuar",
+		],
+		{ capture: true },
+		async (ctx, { state, flowDynamic }) => {
+			const { body } = ctx;
+			await state.update({ documento: body });
+			await flowDynamic("Gracias por darme tu nombre");
+		}
+	)
+
+	.addAnswer(["¿En que puedo ayudarte?"], {
 		capture: true,
 		buttons: [
 			{ body: "Reclamo" },
 			{ body: "Sugerencia" },
 			{ body: "Consulta" },
 		],
-	}
-);
+	});
 
 const consultaFlow = addKeyword("consulta").addAnswer(
 	"Escribe tu consulta",
