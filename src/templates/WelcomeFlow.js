58f63d3788d64d924eb1e060c8351d095f430e8e
arielAayala <ariel170802@gmail.com>
2024-12-09 08:10:05 -0300

    custom payload response

--- src/templates/WelcomeFlow.js
+++ src/templates/WelcomeFlow.js
@@ -10,22 +10,41 @@ const welcomeFlow = addKeyword(EVENTS.WELCOME).addAction(async (ctx, ctxFn) => {
 
 	let response = await textQuery(ctx.body, ctx.from);
 
-	console.log(response);
-
-	if (!response[0].queryResult.fulfillmentText) {
+	if (!response) {
 		return ctxFn.flowDynamic(
 			"Oops, disculpe... Por el momento, no esta funcionando el sistema"
 		);
 	}
-	await state.update({ answer: response[0].queryResult.fulfillmentText });
+
+	await state.update({
+		message:
+			response[0].queryResult.fulfillmentMessages[1]?.payload.fields.response
+				.structValue.fields || response[0].queryResult.fulfillmentText,
+	});
 	return gotoFlow(dialogFlow);
 });
 
 const dialogFlow = addKeyword(EVENTS.ACTION).addAction(
 	async (_, { state, flowDynamic }) => {
-		const currentState = state.getMyState();
+		try {
+			const currentState = state.getMyState();
 
-		await flowDynamic(currentState.answer);
+			await flowDynamic(
+				currentState.message.response?.stringValue || currentState.message
+			);
+			if (currentState.message.question?.stringValue) {
+				await flowDynamic([
+					{
+						body: currentState.message.question?.stringValue,
+						buttons: currentState.message.button?.listValue.values.map((i) => {
+							return { body: i.stringValue };
+						}),
+					},
+				]);
+			}
+		} catch (error) {
+			await flowDynamic("Oops... Ha ocurrido un error");
+		}
 	}
 );
 
